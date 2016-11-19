e4f2abb6dd46fd92fb26b11d027851560c8bfc85
Luke <lukeamward@gmail.com>
2016-11-19 15:47:31 +0000

    Move bootstrap carousel to home page.
    Add action for brand click on nav menu

--- app/assets/js/navbar-nav-active.js
+++ app/assets/js/navbar-nav-active.js
@@ -2,3 +2,8 @@ $(".nav a").on("click", function () {
     $(".nav").find(".active").removeClass("active");
     $(this).parent().addClass("active");
 });
+
+$(".navbar-brand").on("click", function () {
+    $(".nav").find(".active").removeClass("active");
+    $(".nav").find('li').eq(0).addClass("active");
+});
